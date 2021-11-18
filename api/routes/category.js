bb4fe0d86e785676c8c0f560e40cf45af68d7ce5
laetitia123 <uwizelaetitia@gmail.com>
2021-11-18 15:04:13 +0200

    CATEGORY

--- api/routes/category.js
+++ api/routes/category.js
@@ -1,26 +1,6 @@
 const router = require("express").Router();
 const Category = require("../models/Category");
 
-<<<<<<< HEAD
-router.post("/", async (req, res) => {
-  const newCat = new Category(req.body);
-  try {
-    const savedCat = await newCat.save();
-    res.status(200).json(savedCat);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/", async (req, res) => {
-    try {
-      const cats = await Category.find();
-      res.status(200).json(cats);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-=======
 router.post("/create", async (req, res) => {
   const newCat = new Category(req.body);
   try {
@@ -39,6 +19,5 @@ router.get("/all", async (req, res) => {
     return res.status(500).json(err);
   }
 });
->>>>>>> 9344f642762c9903386ceef2cc2a2f2abbbd046a
 
 module.exports = router;
