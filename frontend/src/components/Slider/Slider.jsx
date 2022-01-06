ffbdf5fb1acf0fc2b11e3d840a11d077f93660d8
laetitia123 <uwizelaetitia@gmail.com>
2022-01-06 15:26:28 +0200

    slider

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -7,22 +7,25 @@ import { Carousel } from "antd";
 export default function Slider() {
   return (
 
-    <Carousel autoplay>
-    xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    <Carousel autoplay effect="fade">
+
     <div className="slider">
       <div class="sliderImage">
         <div className="sliderTitles">
           <span className="smallTitle">FEATURED POST</span>
 
-          <span className="largeTitle">
+          {/* <span className="largeTitle">
             SheCanCode aims at creating a vibrant community of amazing young
             Rwanda women who are passionate about using technology to change
             Africa and beyond.
-          </span>
+          </span> */}
+          <p className="smallParagraph">24 JAN 2022</p>
+          <button className="smallButton">READ</button>
+
         </div>
-        {/* <img className="sliderImage" 
+         <img className="sliderImage" 
             src="https://images.pexels.com/photos/459654/pexels-photo-459654.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"/>
-           */}
+           
       </div>
     </div>
       <div className="slider">
@@ -30,15 +33,17 @@ export default function Slider() {
           <div className="sliderTitles">
             <span className="smallTitle">FEATURED POST</span>
 
-            <span className="largeTitle">
+            {/* <span className="largeTitle">
               SheCanCode aims at creating a vibrant community of amazing young
               Rwanda women who are passionate about using technology to change
               Africa and beyond.
-            </span>
+            </span> */}
+           <p className="smallParagraph">24 JAN 2022</p>
+          <button className="smallButton">READ</button>
           </div>
-          {/* <img className="sliderImage" 
-              src="https://images.pexels.com/photos/459654/pexels-photo-459654.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"/>
-             */}
+          <img className="sliderImage" 
+              src="https://pbs.twimg.com/media/FGF_CEAXsAUxVeV?format=jpg&name=large"/>
+            
         </div>
       </div>
     </Carousel>
