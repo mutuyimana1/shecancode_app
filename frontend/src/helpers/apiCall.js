ea0514c3f2f0c0155e1d0d2f083c3ad46ee9cd1b
KAYITARES <cyntkayitare@example.com>
2022-07-12 13:58:45 +0200

    change link

--- frontend/src/helpers/apiCall.js
+++ frontend/src/helpers/apiCall.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
+import axios from "axios";
+import dotenv from "dotenv";
 
 dotenv.config("../.env");
-export default "https://shecancode-blog.herokuapp.com/api";
+export default "https://shecancode-blogs-api.herokuapp.com/api";
 //export default "http://localhost:4048/api";
