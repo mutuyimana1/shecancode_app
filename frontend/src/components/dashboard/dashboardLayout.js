fab22826f0f86eb61c2f97ca3e859a0b2ccd6427
KAYITARES <cyntkayitare@gmail.com>
2023-01-31 17:02:17 +0200

    fix conflict

--- frontend/src/components/dashboard/dashboardLayout.js
+++ frontend/src/components/dashboard/dashboardLayout.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from "@mui/material/Grid";
 import "./dashstyle.css";
 import Sidebar from "./sideBar";
 
